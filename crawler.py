899fb28f0b42d8551bd89c5fb0803dc388a07901
qdx <qdxzzz@gmail.com>
2014-02-05 20:43:21 -0800

    basic parse function wrote, parsing trs out of table

--- crawler.py
+++ crawler.py
@@ -3,13 +3,19 @@ import codecs
 #import bs4
 from bs4 import BeautifulSoup
 
+
+def parse_table(table):
+    f = codecs.open("./data/testout.html", 'w', 'utf-8')
+    for tr in table.find_all('tr'):
+        f.write(tr.prettify())
+    f.close()
+
 minion_page = requests.get("http://hearthstone.gamepedia.com/Minion")
 
 soup = BeautifulSoup(minion_page.text)
 for n in soup.find_all(class_="narrowonly"):
     n.decompose()
 cards = soup.find(class_="cardtable")
-f = codecs.open("./testout.html",'w','utf-8')
-f.write(cards.prettify())
-f.close()
+parse_table(cards)
+
 
