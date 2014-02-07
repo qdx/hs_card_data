b9dc5c475c60830e3d5a226128edbb0cc1751c13
qdx <qdxzzz@gmail.com>
2014-02-06 21:44:52 -0800

    basic game components

---
+++ HSGame.py
@@ -0,0 +1,13 @@
+'''
+Logic
+Looks like each effect a card own is separated by "."
+Game Elements:
+    1. Battle field where minions stand
+    2. Players' heros
+    3. Players' decks
+    4. Players' hands
+    5. Turns
+    6. History(in turn and overall)
+    7. Magic crystals
+    8. Player's weapons
+'''
